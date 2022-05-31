103a2a26d94ee557ea218bb2bed2c3034bad6308
jordy2015 <jordy.g2012@gmail.com>
2022-05-30 20:24:06 -0600

    feat(favourites): Do persistent favourites

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,4 +1,6 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
@@ -14,6 +16,32 @@ export const FavouritesContextProvider = ({ children }) => {
     );
     setFavourites(newFavourites);
   };
+
+  const saveFavourites = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@favourites", jsonValue);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const loadFavourites = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("@favourites");
+      return jsonValue != null ? setFavourites(JSON.parse(jsonValue)) : null;
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
 
   return (
     <FavouritesContext.Provider
